033b758e8ac4959d214839da0a16c76ba611d21c
hushicai <bluthcy@gmail.com>
2015-03-27 15:37:43 +0800

    first commit

--- test.js
+++ test.js
@@ -1,3 +1,36 @@
-(function () {
-    alert(1);
-})();
+(function(){
+    "use strict";
+
+    var logElement = document.getElementById('log-content');
+    function log(content) {
+        logElement.innerHTML += '<p>' + content + '</p>';
+    }
+    function clear() {
+        logElement.innerHTML = '';
+    }
+    var logBtn = document.getElementById('clear');
+    logBtn.onclick = clear;
+
+    var spans = document.getElementsByTagName("span");
+
+    function cal() {
+        for(var i = 0; i < spans.length; i += 1) {
+            var c = spans[i].getAttribute('data-code');
+            var v = spans[i].innerHTML;
+            var n = eval(c);
+
+            if (v !== '' && v !== n) {
+                log(c + ': chanaged!');
+            };
+
+            spans[i].innerHTML = n;
+        }
+    }
+
+    window.onresize = function () {
+        log('resized');
+        cal();
+    };
+
+    cal();
+}());
