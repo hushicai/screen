2fa9143fe9854edcae6ecf952a1107876951a117
hushicai <bluthcy@gmail.com>
2015-03-27 16:45:40 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # screen
 
-测试移动设备在软键盘弹起时，viewport是否会发生改变。
+测试移动设备在软键盘弹起时，viewport各项dimensions是否发生改变。
 
 ## 测试地址 
 
--- test.js
+++ test.js
@@ -1,26 +1,23 @@
 (function(){
     "use strict";
 
-    var logElement = document.getElementById('log-content');
+    var logElement = document.getElementById('log');
     function log(content) {
         logElement.innerHTML += '<p>' + content + '</p>';
     }
     function clear() {
         logElement.innerHTML = '';
     }
-    var logBtn = document.getElementById('clear');
-    logBtn.onclick = clear;
 
     var spans = document.getElementsByTagName("span");
-
     function cal() {
         for(var i = 0; i < spans.length; i += 1) {
             var c = spans[i].getAttribute('data-code');
             var v = spans[i].innerHTML;
             var n = eval(c);
 
-            if (v !== '' && v !== n) {
-                log(c + ': chanaged!');
+            if (v !== '' && parseInt(v, 10) !== n) {
+                log(c + ' chanaged! old: ' + v + ', new: ' + n);
             };
 
             spans[i].innerHTML = n;
@@ -29,8 +26,14 @@
 
     window.onresize = function () {
         log('resized');
-        cal();
+        // cal();
     };
 
     cal();
+
+    var input = document.getElementById('input');
+    input.onfocus = function () {
+        clear();
+        cal();
+    };
 }());
